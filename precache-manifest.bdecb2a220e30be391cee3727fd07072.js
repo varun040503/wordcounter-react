e43c0560ba74f9add11dd1745776dba6914b1e8e
varun040503 <varunreddy2439@gmail.com>
2023-06-21 22:50:05 +0530

    Updates

---
+++ precache-manifest.bdecb2a220e30be391cee3727fd07072.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "98f33d4ea6e714d2f7e8",
+    "url": "/wordcounter-react/static/css/main.e2c48b99.chunk.css"
+  },
+  {
+    "revision": "98f33d4ea6e714d2f7e8",
+    "url": "/wordcounter-react/static/js/main.98f33d4e.chunk.js"
+  },
+  {
+    "revision": "6a81eab60fd77029b580",
+    "url": "/wordcounter-react/static/js/1.6a81eab6.chunk.js"
+  },
+  {
+    "revision": "5e8601a449736e1fdc42",
+    "url": "/wordcounter-react/static/js/2.5e8601a4.chunk.js"
+  },
+  {
+    "revision": "0427d538eb44e519e1c9",
+    "url": "/wordcounter-react/static/js/runtime~main.0427d538.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/wordcounter-react/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "ff1adb43c1d5b207f6ead003281bfbd9",
+    "url": "/wordcounter-react/index.html"
+  }
+];
